853cf4bb7c9668fc88bfb3b7f970fe62c4ef9a3f
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-03-29 20:22:51 +0800

    added ability to show url based on the short

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
+# Represents a URL that can be shortened to a unique, randomly generated identifier.
+# Public methods:
+# - to_param: Returns short attribute as the parameter for url
 class Url < ApplicationRecord
+  # Returns short attribute as the parameter for url
+  def to_param
+    short
+  end
 end
