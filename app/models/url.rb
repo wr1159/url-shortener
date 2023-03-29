377e593e183e1141aeee0cc6f3124f784743eb6a
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-03-29 23:50:35 +0800

    Added target url validation

--- app/models/url.rb
+++ app/models/url.rb
@@ -6,11 +6,20 @@
 class Url < ApplicationRecord
 
   # Validations
-  validates :target, presence: { message: 'Please enter a URL'}
+  validates :target, presence: true, format: { with: URI.regexp(%w[http https]), message: 'must be a valid URL' }
   validates :short, presence: true, uniqueness: true
+  before_validation :normalize_target
 
   # Returns short attribute as the parameter for url
   def to_param
     short
+  end
+
+  private 
+
+  def normalize_target
+    return if target.blank?
+
+    self.target = "http://#{target}" if target.match?(/\Awww\./) && !target.start_with?("http://", "https://")
   end
 end
