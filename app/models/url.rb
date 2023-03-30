e14c99c6da3d2a8cde155844c93753d176e24af5
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-03-31 02:02:37 +0800

    Added title extraction + improved target url validation

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
+require 'net/http'
 # Represents a URL that can be shortened to a unique, randomly generated identifier.
 # Public methods:
 # - to_param: Returns short attribute as the parameter for url
+# - normalize_target: Adds https:// to target if not present and contains www.
+# - validate_target_url: Ensures that link is valid by getting the title tag using regex
 class Url < ApplicationRecord
 
   # Validations
-  validates :target, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: 'must be a valid URL' }
+  validates :target, presence: true, format: {  with: /\A(https?):\/\/[^\s\/$.?#].[^\s]*\z/i, message: 'must be a valid URL' }
+  validate :validate_target_url
   validates :short, presence: true, uniqueness: true
+  validates :title, presence: true
   before_validation :normalize_target
 
   # Returns short attribute as the parameter for url
@@ -20,6 +25,28 @@ class Url < ApplicationRecord
   def normalize_target
     return if target.blank?
 
-    self.target = "http://#{target}" if target.match?(/\Awww\./) && !target.start_with?("http://", "https://")
+    self.target = "https://#{target}" if target.match?(/\Awww\./) && !target.start_with?("http://", "https://")
+  end
+
+  def validate_target_url
+    return errors.add(:target, 'is not valid URL') unless target =~ /\A(https?):\/\/[^\s\/$.?#].[^\s]*\z/i
+
+    uri = URI.parse(target)
+    response = Net::HTTP.get_response(uri)
+    # Loop to account for redirecting websites
+    loop do
+      response = Net::HTTP.get_response(uri) 
+      # If it's a redirect, update the URI and try again
+      if response.is_a?(Net::HTTPRedirection)
+        uri = URI.parse(response['location'])
+      else
+        break
+      end
+    end
+    if response.is_a?(Net::HTTPSuccess)
+      self.title = response.body.match(/<title>(.*?)<\/title>/)[1]
+    else
+      errors.add(:target, 'is not valid target')
+    end
   end
 end
