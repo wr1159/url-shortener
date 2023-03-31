75c76a577c516caf7019ce66cfc45817e0944958
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-04-01 00:54:54 +0800

    Revert "Added tests for UrlVisits"
    This reverts commit 4cb960616e33d35cd28f0bca3fab07dee0ba9575.
    Pushed app/views/new.html.erb when it is not ready

--- spec/models/url_visit_spec.rb
+++ spec/models/url_visit_spec.rb
@@ -1,29 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe UrlVisit, type: :model do
-  let(:url) { Url.create(target: 'https://example.com', short: 'example') }
-
-  describe 'associations' do
-    it { is_expected.to belong_to(:url) }
-  end
-
-  describe 'creation' do
-    it 'creates a new url visit' do
-      expect {
-        url.url_visits.create(country: 'Malaysia', city: 'Kuala Lumpur')
-      }.to change { UrlVisit.count }.by(1)
-    end
-
-    it 'creates a new url visit without city' do
-      expect {
-        url.url_visits.create(country: 'Malaysia')
-      }.to change { UrlVisit.count }.by(1)
-    end
-
-    it 'creates a new url visit without country' do
-      expect {
-        url.url_visits.create(city: 'Kuala Lumpur')
-      }.to change { UrlVisit.count }.by(1)
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
